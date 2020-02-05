003c4b9a8cbb70e52736c74cc8a9bff264bc92c0
Matej Celarc <matej@Matejs-MacBook-Air.local>
2020-02-05 23:59:54 +0100

    add process.env.PUBLIC_URL as basename to BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 const App = () => {
     return (
         <div className="wrapper">
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <Route key="root-navigation" path="/" component={NavigationBar} />
                 <Route key="root-categories" path="/" exact component={CategoriesList} />
                 <Route key="genre" path="/genre/:genreId/:genreName" component={CategoriesList} />
