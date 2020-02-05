fa57ae6284067ab3b1bb4eb4407d1eab198698c0
Matej Celarc <matej@Matejs-MacBook-Air.local>
2020-02-06 00:01:02 +0100

    Updates

---
+++ precache-manifest.a1d8e561a7a30ab7d06c7f07ccc2a6e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d04a47a35591851d6d2eaf57a8d84551",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e325468c19e5ef17e885",
+    "url": "/static/js/2.bc0e6efa.chunk.js"
+  },
+  {
+    "revision": "0ab5348bc1a487968b15",
+    "url": "/static/js/main.9498779b.chunk.js"
+  },
+  {
+    "revision": "4339cf47e6eedab1b122",
+    "url": "/static/js/runtime-main.624fbbe2.js"
+  }
+]);
