ca56bbf9172858299eafad98a7fc1bf2049cfcb1
Matej Celarc <matej@Matejs-MacBook-Air.local>
2020-02-05 23:49:57 +0100

    add searchResults to store with searchReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import moviesByGenreReducer from './moviesByGenreReducer';
 import moviesReducer from './moviesReducer';
+import searchReducer from './searchReducer';
 
 export default combineReducers({ 
     genreMovies: moviesByGenreReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    searchResults: searchReducer
  });
