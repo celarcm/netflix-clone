19757dac6ed0ff07d60b6226a28f0472ef93e24d
Matej Celarc <matej@Matejs-MacBook-Air.local>
2020-02-05 23:48:58 +0100

    Add search functionality, add hardcoded Genres to navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,27 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 class NavigationBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            redirect: '',
+            searchSubmitted: false
+        }
+    }
+    handleSubmit(e) {
+        if (e.key === 'Enter') {
+            console.log('Pressed Enter in search,', e.target.value);
+            this.setState({
+                redirect: `/search/${e.target.value}`,
+                searchSubmitted: true
+            });
+        }
+    }
     render() {
-        return (
-            <header>
-                <div class="netflixLogo">
-                    <a id="logo" href="#home"><img src="https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="Logo Image" /></a>
-                </div>      
-                <nav class="main-nav">    
-                    <Link to="/">Home</Link>
-                    <Link to="/genre/action">Action</Link>
-                    <Link to="/genre/comedies">Comedies</Link>
-                    <Link to="/genre/dramas">Dramas</Link>
-                    <Link to="/genre/kids">Kids</Link>
-                    <Link to="/genre/scifi">Sci-Fi</Link>
-                </nav>
-                <nav class="sub-nav">
-                    <input class="search-box"></input>
-                    <a href="#"><i class="fas fa-search sub-nav-logo"></i></a>
-                </nav>      
-            </header>
-        )
+        if (this.state.searchSubmitted) {
+            return <Redirect to={this.state.redirect}/>
+        } else {
+            return (
+                <header>
+                    <div class="netflixLogo">
+                        <a id="logo" href="#home"><img src="https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="" /></a>
+                    </div>      
+                    <nav class="main-nav">    
+                        <Link to="/">Home</Link>
+                        <Link to="/genre/28/Action">Action</Link>
+                        <Link to="/genre/35/Comedy">Comedy</Link>
+                        <Link to="/genre/18/Drama">Drama</Link>
+                        <Link to="/genre/10751/Family">Family</Link>
+                        <Link to="/genre/878/SciFi">SciFi</Link>
+                    </nav>
+                    <nav class="sub-nav">
+                        <input class="search-box" onKeyPress={this.handleSubmit.bind(this)}></input>
+                        <a><i class="fas fa-search sub-nav-logo"></i></a>
+                    </nav>      
+                </header>
+            )
+        }
     };
 };
 
