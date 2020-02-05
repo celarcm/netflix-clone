9c03faea607806b409b26c5983092c3a1070c6c6
Matej Celarc <matej@Matejs-MacBook-Air.local>
2020-02-05 23:39:48 +0100

    fix dispatch payloads, fix searhMovies url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,7 @@
 import movieDBApi from '../apis/movieDBApi';
 
 export const fetchMoviesByGenre = (genreId) => async dispatch => {
-    console.log('inside fetchMoviesByGenre');
     const response = await movieDBApi.get(`discover/movie?with_genres=${genreId}`);
-
-    console.log('response', response);
 
     dispatch({ type: 'FETCH_MOVIES_BY_GENRE', payload: {
         genreId: genreId,
@@ -15,11 +12,11 @@ export const fetchMoviesByGenre = (genreId) => async dispatch => {
 export const fetchMovie = (movieId) => async dispatch => {
     const response = await movieDBApi.get(`/movie/${movieId}`);
 
-    dispatch({ type: 'FETCH_MOVIE', payload: response.result });
+    dispatch({ type: 'FETCH_MOVIE', payload: response.data.result });
 };
 
 export const searchMovies = (searchString) => async dispatch => {
-    const response = await movieDBApi.get(`/search/${searchString}`);
+    const response = await movieDBApi.get(`/search/movie?query=${searchString}`);
 
-    dispatch({ type: 'SEARCH_MOVIES', payload: response.result });
+    dispatch({ type: 'SEARCH_MOVIES', payload: response.data.results });
 };
