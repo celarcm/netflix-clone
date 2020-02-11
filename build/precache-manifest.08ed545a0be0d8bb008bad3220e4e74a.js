bf6038b5e5e41d23348e584129ab346c3965b595
Matej Celarc <matej@Matejs-MacBook-Air.local>
2020-02-11 15:06:39 +0100

    new build

---
+++ build/precache-manifest.08ed545a0be0d8bb008bad3220e4e74a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd4418acc32c44c42ec1b459981ee4c",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "8e56711dceea5b277f0e",
+    "url": "/netflix-clone/static/js/2.dc6026e7.chunk.js"
+  },
+  {
+    "revision": "aa1c43bb49cac50f2eae",
+    "url": "/netflix-clone/static/js/main.cf818e35.chunk.js"
+  },
+  {
+    "revision": "c12239ce37017e83f07e",
+    "url": "/netflix-clone/static/js/runtime-main.6a01a38f.js"
+  }
+]);
